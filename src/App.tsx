7296ca2e7723959865217a723ff481a377f22294
sirlordluis <lordbanana1@gmail.com>
2023-07-11 18:19:34 -0600

    initial commit!

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from 'react';
+import './styles/App.css';
+import ScrollContent from './components/ScrollContent';
+import Navbar from './components/Navbar';
+
+function App() {
+  const appRef = useRef(null);
+/*COLOCAR EL GRADIENTE AL INICIO*/
+  useEffect(() => {
+    const setGradiantBackground = () => {
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+
+      const mouseX = Math.round((winWidth / 2) * 100);
+      const mouseY = Math.round((winHeight / 2) * 100);
+
+      if (appRef.current) {
+        appRef.current.style.setProperty('--mouse-x', mouseX.toString() + '%');
+        appRef.current.style.setProperty('--mouse-y', mouseY.toString() + '%');
+      }
+    };
+/*USAR GRADIENTE AL MOVER EL MOUSE*/
+    setGradiantBackground();
+
+    const moveGradiant = (event) => {
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+
+      const mouseX = Math.round((event.pageX / winWidth) * 100);
+      const mouseY = Math.round((event.pageY / winHeight) * 100);
+
+      if (appRef.current) {
+        appRef.current.style.setProperty('--mouse-x', mouseX.toString() + '%');
+        appRef.current.style.setProperty('--mouse-y', mouseY.toString() + '%');
+      }
+    };
+    document.addEventListener('mousemove', moveGradiant);
+    return () => {
+      document.removeEventListener('mousemove', moveGradiant);
+    };
+  });
+
+  return (
+    <>
+      
+      <div className="app" id="app" ref={appRef} data-scroll-container>
+        <div className="blink justify-center flex align-center">
+          
+        </div>
+      </div>
+      
+      <h1 className='custom-font'>Welcome!</h1>
+      <Navbar/>
+      <ScrollContent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ScrollContent.tsx
@@ -0,0 +1,13 @@
+import '../styles/ScrollContent.css'
+
+const ScrollContent = () => {
+  // Contenido que deseas mostrar al hacer scroll
+  return (
+    
+      <p>Hola, Esta es mi pagina web principal starter </p>
+      
+    
+  );
+};
+
+export default ScrollContent;
